c87c9e04c07a2e6aa316bd54cf9b8d2def1e7822
Nicolas Comaschi <nicolascomaschi@gmail.com>
2019-11-06 11:04:20 -0300

    new

---
+++ Logica/Helpers.cs
@@ -0,0 +1,22 @@
+﻿using Datos;
+using System;
+
+namespace Logica
+{
+    public class Helpers
+    {
+        public bool Guardar(ClinicaEntities ctx)
+        {
+            try
+            {
+                ctx.SaveChanges();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ Logica/LObraSocial.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data.Entity;
+using Datos;
+
+namespace Logica
+{
+    public class LObraSocial
+    {
+        ClinicaEntities ctx = new ClinicaEntities();
+        public void Insertar(string nombre, string detalle)
+        {
+            ObraSocial obj = new ObraSocial
+            {
+                nombre = nombre,
+                detalles = detalle,
+            };
+            ctx.ObraSocial.Add(obj);
+            ctx.SaveChanges();
+        }
+    }
+}
